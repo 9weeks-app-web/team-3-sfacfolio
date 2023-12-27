511a60dc96d891a33d84692f1da91d52ab1eed8a
seonnn <ssni0105@gmail.com>
2023-12-27 14:39:19 +0900

    Feat: footer 컴포넌트 관련 이미지 파일 추가

---
+++ sfacfolio/public/assets/images/SocialIcons/index.ts
@@ -0,0 +1,9 @@
+import youtube from './Youtube.svg';
+import facebook from './Facebook.svg';
+import instagram from './Instagram.svg';
+
+export const socialIcons = {
+  youtube,
+  facebook,
+  instagram,
+};
