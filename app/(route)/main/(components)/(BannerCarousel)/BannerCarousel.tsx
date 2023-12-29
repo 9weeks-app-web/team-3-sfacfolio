ecc24e8bdfe511cead2b51f6be4ffa9751e58d65
kangkyeongseo <kks_big@naver.com>
2023-12-29 17:33:54 +0900

    Feat: 배너 캐러셀 컴포넌트 Props 추가

--- app/(route)/main/(components)/(BannerCarousel)/BannerCarousel.tsx
+++ app/(route)/main/(components)/(BannerCarousel)/BannerCarousel.tsx
@@ -3,7 +3,11 @@ import Image from 'next/image';
 import Slider from 'react-slick';
 import styles from '@/style/bannerCarousel.module.css';
 
-function BannerCarousel() {
+interface BannerCarouselPropsType {
+  banners?: string[];
+}
+
+function BannerCarousel({ banners }: BannerCarouselPropsType) {
   const settings = {
     dots: true,
     speed: 500,
@@ -20,15 +24,23 @@ function BannerCarousel() {
       className={`${styles.wrapper} max-w-[1440px] mx-auto  pl-20 pt-20 pb-[124px] bg-background-primary`}
     >
       <Slider {...settings}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <div
-            key={i}
-            style={{ width: 604 }}
-            className='h-[300px] bg-background-secondary rounded-lg'
-          >
-            {/* <Image src={} alt=''/> */}
-          </div>
-        ))}
+        {banners
+          ? banners.map(banner => (
+              <div
+                key={banner}
+                style={{ width: 604 }}
+                className='cursor-pointer h-[300px] bg-background-secondary rounded-lg'
+              >
+                <Image src={banner} alt='banner-image' objectFit='cover' />
+              </div>
+            ))
+          : [1, 2, 3, 4, 5].map(i => (
+              <div
+                key={i}
+                style={{ width: 604 }}
+                className='cursor-pointer h-[300px] bg-background-secondary rounded-lg'
+              ></div>
+            ))}
       </Slider>
     </div>
   );
