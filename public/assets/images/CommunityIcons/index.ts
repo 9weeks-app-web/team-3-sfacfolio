cedddf05c02e14a324beb5d2694f05ec10662de7
kangkyeongseo <kks_big@naver.com>
2023-12-29 13:19:45 +0900

    Feat: 실시간 인기 커뮤니티 섹션 관련 이미지 파일 추가

---
+++ public/assets/images/CommunityIcons/index.ts
@@ -0,0 +1,11 @@
+import comment from './Comment.svg';
+import heart from './Heart.svg';
+import risePolygon from './RisePolygon.svg';
+import declinePolygon from './DeclinePolygon.svg';
+
+export const communityIcons = {
+  comment,
+  heart,
+  risePolygon,
+  declinePolygon,
+};
