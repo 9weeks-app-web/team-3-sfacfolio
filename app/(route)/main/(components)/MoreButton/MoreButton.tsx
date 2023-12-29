25157a36855038b40901db52f03a4d5bbbf06431
geonwooPark <white0581@naver.com>
2023-12-29 16:13:25 +0900

    Feat: 더보기 버튼 컴포넌트 추가

---
+++ app/(route)/main/(components)/MoreButton/MoreButton.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import React from 'react';
+import IconPlus from '../../../public/assets/images/IconPlus.svg';
+
+interface MoreButtonProps {
+  label: string;
+}
+
+export default function MoreButton({ label }: MoreButtonProps) {
+  return (
+    <button className='flex items-center rounded-[6px] border border-line-normal px-6 py-4'>
+      <span className='mr-1 text-[18px] font-medium leading-6 text-text-alternative'>
+        {label}
+      </span>
+      <span>
+        <Image src={IconPlus} alt='plus-icon' />
+      </span>
+    </button>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
+
+  // 임시로 설정
+  images: {
+    domains: ['s3-alpha-sig.figma.com'],
+  },
 };
 
 module.exports = nextConfig;
