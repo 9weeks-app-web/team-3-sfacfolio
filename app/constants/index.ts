f2308bbb53dbba044fc0011287c79735b68390af
geonwooPark <white0581@naver.com>
2024-01-02 14:40:01 +0900

    Feat: Footer 컴포넌트 수정

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,32 +1,33 @@
-export const footerInfo = [
+export const footerInfo1 = [
   {
     id: 1,
     text: '인사이드아웃 사회적 협동조합',
   },
   {
     id: 2,
-    text: '이사장 : 염민호 (와이엠에스닷코)',
-  },
-  {
-    id: 3,
-    text: '사업자 등록번호 : 450-87-01864',
-  },
-  {
-    id: 4,
     text: '주소 : 서울특별시 강서구 마곡중앙2로 11 305호',
   },
   {
-    id: 5,
+    id: 3,
     text: '연락처 : 050-6683-1001',
-    icon: false,
   },
   {
-    id: 6,
+    id: 4,
+    text: '고객센터 : cs@sniperfactory.com',
+  },
+];
+
+export const footerInfo2 = [
+  {
+    id: 5,
     text: '통신판매업 신고번호 : 2022-경기김포-3659',
   },
   {
+    id: 6,
+    text: '이사장 : 염민호 (와이엠에스닷코)',
+  },
+  {
     id: 7,
-    text: '고객센터 : cs@sniperfactory.com',
-    icon: false,
+    text: '사업자 등록번호 : 450-87-01864',
   },
 ];
