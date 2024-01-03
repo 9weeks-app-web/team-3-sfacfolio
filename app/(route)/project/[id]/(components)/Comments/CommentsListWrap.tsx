b0668ff320f7c24d451fadf9ec3a54d02e83b752
Gahee Kim <soprue@naver.com>
2024-01-03 20:57:27 +0900

    Feat: comment 컴포넌트 재구성

---
+++ app/(route)/project/[id]/(components)/Comments/CommentsListWrap.tsx
@@ -0,0 +1,14 @@
+import { CommentType } from '@/types/project';
+import { CommentsWrapProps } from './CommentsWrap';
+
+import CommentsBox from './CommentsBox';
+
+export default function CommentsListWrap({ comments }: CommentsWrapProps) {
+  return (
+    <div className='my-14 flex w-full flex-col items-end'>
+      {comments.map((comment: CommentType) => (
+        <CommentsBox comment={comment} key={comment.id} />
+      ))}
+    </div>
+  );
+}
--- app/(route)/project/[id]/(components)/Comments/CommentsWrap.tsx
+++ app/(route)/project/[id]/(components)/Comments/CommentsWrap.tsx
@@ -1,17 +1,29 @@
 import { CommentType } from '@/types/project';
 
-import CommentsBox from './CommentsBox';
+import ProjectSectionTitle from '../Project/ProjectSectionTitle';
+import CommentsListWrap from './CommentsListWrap';
 
-interface CommentsWrapProps {
+export interface CommentsWrapProps {
   comments: CommentType[];
 }
 
 export default function CommentsWrap({ comments }: CommentsWrapProps) {
   return (
-    <div className='my-14 flex w-full flex-col items-end'>
-      {comments.map((comment: CommentType) => (
-        <CommentsBox comment={comment} key={comment.id} />
-      ))}
+    <div className='mt-8'>
+      <ProjectSectionTitle title='댓글' number={4} />
+
+      <form className='flex justify-between'>
+        <input
+          type='input'
+          placeholder='댓글을 입력하세요.'
+          className='h-12 w-[calc(100%-144px-10px)] rounded-lg border border-line-normal bg-background-primary px-[14px] py-4 outline-none'
+        />
+        <button className='h-12 w-36 rounded-lg border border-line-normal bg-background-secondary'>
+          등록
+        </button>
+      </form>
+
+      <CommentsListWrap comments={comments} />
     </div>
   );
 }
