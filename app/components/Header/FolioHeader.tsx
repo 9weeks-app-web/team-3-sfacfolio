ac6981c749a924707963e8d5283a4fce8e7b0f58
seonnn <ssni0105@gmail.com>
2024-01-03 16:06:29 +0900

    Feat: 헤더 컴포넌트 경로 이동 기능 추가

--- app/components/Header/FolioHeader.tsx
+++ app/components/Header/FolioHeader.tsx
@@ -1,13 +1,14 @@
 'use client';
 import React from 'react';
-import { useSelectedLayoutSegment } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import FolioHeaderInput from './FolioHeaderInput';
 import LoginButton from './LoginButton';
 import Image from 'next/image';
 import LogoFolio from '@images/LogoFolio.svg';
+import Link from 'next/link';
 
 const menuItems = [
-  { name: '홈', segment: null }, // segment 'home'으로 수정 필요
+  { name: '홈', segment: 'main' },
   { name: '포트폴리오', segment: 'portfolio' },
   { name: '프로젝트', segment: 'project' },
   { name: '채용', segment: 'recruit' },
@@ -15,8 +16,7 @@ const menuItems = [
 ];
 
 function FolioHeader() {
-  // TODO: 경로 설정 방식 확인 후 segment에 따라 css 적용
-  const segment = useSelectedLayoutSegment();
+  const pathname = usePathname();
 
   return (
     <div className='box-border flex h-14 w-full justify-between border-b-2 border-b-line-normal bg-background-secondary px-10'>
@@ -24,16 +24,18 @@ function FolioHeader() {
         <Image src={LogoFolio} alt='logo' width={63.76} height={16.14} />
         <ul className='ml-6 flex text-label1 leading-4 text-text-alternative'>
           {menuItems.map(item => (
-            <li
-              key={item.name}
-              className={`relative flex h-14 w-[88px] cursor-pointer items-center justify-center px-2 py-3 tracking-[-0.16px] ${
-                segment === item.segment
-                  ? ' text-text-normal before:absolute before:bottom-0 before:block before:h-[2px] before:w-full before:bg-primary-heavy'
-                  : ''
-              }`}
-            >
-              <span className='w-[72px] text-center'>{item.name}</span>
-            </li>
+            <Link key={item.name} href={{ pathname: `/${item.segment}` }}>
+              <li
+                key={item.name}
+                className={`relative flex h-14 w-[88px] cursor-pointer items-center justify-center px-2 py-3 tracking-[-0.16px] ${
+                  pathname.startsWith(`/${item.segment}`)
+                    ? ' text-text-normal before:absolute before:bottom-0 before:block before:h-[2px] before:w-full before:bg-primary-heavy'
+                    : ''
+                }`}
+              >
+                <span className='w-[72px] text-center'>{item.name}</span>
+              </li>
+            </Link>
           ))}
         </ul>
       </div>
