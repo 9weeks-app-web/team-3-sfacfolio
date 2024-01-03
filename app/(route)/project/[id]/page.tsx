c6913546f79e7e35c223f38f53248584f94ec839
Gahee Kim <soprue@naver.com>
2024-01-03 23:52:12 +0900

    Feat: 프로젝트 상세 페이지 상단 정보 컴포넌트 구현

--- app/(route)/project/[id]/page.tsx
+++ app/(route)/project/[id]/page.tsx
@@ -1,30 +1,80 @@
-import ProjectInfoLabel from './(components)/ProjectInfoLabel';
+'use client';
+
+import { useParams } from 'next/navigation';
+import { useQuery } from 'react-query';
+
+import ProjectDetails from './(components)/Project/ProjectDetails';
+import ProjectDescription from './(components)/Project/ProjectDescription';
+import ProjectApplication from './(components)/Project/ProjectApplication';
+import CommentsWrap from './(components)/Comments/CommentsWrap';
+import FixedLayer from './(components)/FixedLayer';
+
+import calculateDaysBeforeDeadline from '@/utils/dateUtils';
+import { fetchProject } from '@/api/project';
+import { ProjectDataType } from '@/types/project';
+import Loader from '@/components/Loader';
+
+interface ProjectQueryResult {
+  data: ProjectDataType | undefined;
+  isLoading: boolean;
+  isError: boolean;
+}
 
 export default function ProjectDetailPage() {
+  const { id } = useParams();
+  const projectId = Array.isArray(id) ? id[0] : id;
+
+  const {
+    data: project,
+    isLoading,
+    isError,
+  } = useQuery(['project', id], () => fetchProject(projectId), {
+    staleTime: 10 * 60 * 1000, // 10분
+    cacheTime: 30 * 60 * 1000, // 30분
+  }) as ProjectQueryResult;
+
+  if (isLoading) {
+    return (
+      <div className='container flex h-screen items-center justify-center p-5 text-center'>
+        <Loader />
+      </div>
+    );
+  }
+  if (!project || isError) {
+    return (
+      <div className='container flex h-screen items-center justify-center p-5 text-center'>
+        데이터 로드 중 오류가 생겼습니다.
+        <br />
+        다시 시도해 주세요.
+      </div>
+    );
+  }
+
+  const leftDay = calculateDaysBeforeDeadline(project?.recruitDeadline);
+
   return (
     <div className='min-h-screen bg-background-primary py-16'>
-      <div className='container rounded-3xl bg-white px-[106px] py-10'>
+      <div className='container relative rounded-3xl bg-white px-[106px] py-10'>
         {/* 프로젝트 정보 Wrapper */}
         <div>
-          {/* 프로젝트 타이틀 */}
-          <div>
-            <div className='flex gap-[10px]'>
-              <ProjectInfoLabel contents='마감 4일 전' type='period' />
-              <ProjectInfoLabel contents='사이드 프로젝트' />
-              <ProjectInfoLabel contents='중급' />
-            </div>
-          </div>
-          {/* 프로젝트 정보 */}
-          <div></div>
-          {/* 프로젝트 지원 */}
-          <div></div>
+          <ProjectDetails data={project} leftDay={leftDay} />
+
+          <hr className='my-8' />
+
           {/* 프로젝트 소개 */}
-          <div></div>
+          <ProjectDescription description={project?.description} />
+
           {/* 프로젝트 참여 신청 */}
-          <div></div>
+          <ProjectApplication participants={project?.participants} />
         </div>
+
+        <hr />
+
         {/* 댓글 Wrapper */}
-        <div></div>
+        <CommentsWrap comments={project?.comments} />
+
+        {/* 지원하기 fixed div */}
+        <FixedLayer participants={project?.participants} leftDay={leftDay} />
       </div>
     </div>
   );
