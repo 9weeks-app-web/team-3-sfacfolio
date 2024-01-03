37a40f1963cde7ce3c5a3627aea64c01a4b88a1e
seonnn <ssni0105@gmail.com>
2024-01-03 23:53:22 +0900

    Feat: ProjectBanner 컴포넌트 추가

---
+++ app/(route)/project/(components)/ProjectBanner/ProjectBanner.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import React from 'react';
+import BusinessStrategy from '@images/BusinessStrategy.svg';
+import Close from '@images/Close.svg';
+import ProjectButton from '../common/ProjectButton';
+
+function ProjectBanner() {
+  return (
+    <div className='flex h-[497px] w-full items-center justify-between bg-black px-[100px] text-white'>
+      <div className='w-[568px]'>
+        <h2 className='mb-[10px] text-5xl'>
+          <span className='font-bold'>&lsquo;프로젝트 매칭하기&lsquo;</span>로
+          빠르고 간편하게 시작하세요!
+        </h2>
+        <p className='mb-[49px]'>
+          수 많은 프로젝트 중에서 나에게 필요한 프로젝트를 찾아주는 프로젝트
+          매칭 서비스로
+          <br /> 빠르고 간편하게 경험을 쌓을 수 있습니다.
+        </p>
+        <div className='flex gap-[15px]'>
+          <ProjectButton color='white' size='big'>
+            프로젝트 매칭하기
+          </ProjectButton>
+          <ProjectButton color='transparent' size='big'>
+            프로젝트 만들기
+            <Image src={Close} alt='프로젝트 만들기' width={18} height={18} />
+          </ProjectButton>
+        </div>
+      </div>
+      <Image
+        src={BusinessStrategy}
+        alt={'배너 이미지'}
+        width={668}
+        height={497}
+      />
+    </div>
+  );
+}
+
+export default ProjectBanner;
