95b1177a0e2ab42c6899f16c555553cb81bbba8b
geonwooPark <white0581@naver.com>
2024-01-04 12:46:16 +0900

    Chore: 로그인 및 회원가입 정규식 작성

---
+++ app/constants/regex.ts
@@ -0,0 +1,4 @@
+export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const nameRegex = /^(?=.*[a-z가-힣])[a-z가-힣]{1,10}$/;
+export const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,16}$/;
