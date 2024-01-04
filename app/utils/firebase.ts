04f9d2db76eb77823c0ee9afe66c23d91eaa7831
geonwooPark <white0581@naver.com>
2024-01-04 12:41:52 +0900

    Chore: Firebase 설정 변경 및 React-Hook-Form 설치

--- app/utils/firebase.ts
+++ app/utils/firebase.ts
@@ -1,4 +1,7 @@
 import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
@@ -12,5 +15,7 @@ const firebaseConfig = {
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const storage = getStorage(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, storage };
+export { app, storage, auth, db };
