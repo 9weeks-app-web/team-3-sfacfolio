150bc9584904bcc9fd07f79b929775b2af0a6b95
Gahee Kim <soprue@naver.com>
2024-01-04 17:28:38 +0900

    Feat: getCurrentDateTimeFormatted 함수 생성

--- app/utils/dateUtils.ts
+++ app/utils/dateUtils.ts
@@ -1,4 +1,4 @@
-export default function calculateDaysBeforeDeadline(deadline: string) {
+export function calculateDaysBeforeDeadline(deadline: string) {
   const today = new Date();
   const deadlineDate = new Date(deadline);
 
@@ -6,4 +6,16 @@ export default function calculateDaysBeforeDeadline(deadline: string) {
   const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
 
   return '마감 ' + daysLeft + '일 전';
+}
+
+export function getCurrentDateTimeFormatted(): string {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const day = now.getDate().toString().padStart(2, '0');
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+
+  return `${year}.${month}.${day} ${hours}:${minutes}`;
 }
