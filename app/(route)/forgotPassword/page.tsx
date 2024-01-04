bad427e1adca3895dbe324b3a9d1fbc8727697c3
geonwooPark <white0581@naver.com>
2024-01-04 12:45:16 +0900

    Feat: 비밀번호 찾기 페이지 제작 및 기능 구현

---
+++ app/(route)/forgotPassword/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ForgotPasswordForm from './(component)/ForgotPasswordForm';
+import ForgotPasswordImage from '@images/ForgotPasswordImage.svg';
+import Image from 'next/image';
+
+export default function page() {
+  return (
+    <div className='flex'>
+      <div className='relative h-screen w-[514px] shrink-0 bg-[#EBF2FF]'>
+        <div className='absolute left-5 top-[85px] h-[700px] w-[700px]'>
+          <Image src={ForgotPasswordImage} alt='login-image' fill />
+        </div>
+      </div>
+      <ForgotPasswordForm />
+    </div>
+  );
+}
