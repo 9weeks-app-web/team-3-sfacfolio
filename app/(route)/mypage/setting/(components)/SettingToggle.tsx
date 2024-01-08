e42ee3360e0f1f432704095ecf44eba292d5dab0
Gahee Kim <soprue@naver.com>
2024-01-08 15:41:41 +0900

    Feat: SettingToggle 컴포넌트 생성

---
+++ app/(route)/mypage/setting/(components)/SettingToggle.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ToggleSwitch from './ToggleSwitch';
+
+interface SettingToggleProps {
+  title: string;
+  checked: boolean;
+  onToggle: () => void;
+}
+
+const SettingToggle: React.FC<SettingToggleProps> = ({
+  title,
+  checked,
+  onToggle,
+}) => {
+  return (
+    <div className='flex items-center justify-between'>
+      <p className='text-text-normal'>{title}</p>
+      <ToggleSwitch checked={checked} onChange={onToggle} />
+    </div>
+  );
+};
+
+export default SettingToggle;
