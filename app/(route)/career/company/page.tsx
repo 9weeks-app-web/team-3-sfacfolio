d2b88329b006a2222b115a9540287647aaff09c1
seonnn <ssni0105@gmail.com>
2024-01-08 17:45:39 +0900

    Feat: CompanyPage 내 Filter 버튼 적용

--- app/(route)/career/company/page.tsx
+++ app/(route)/career/company/page.tsx
@@ -6,6 +6,7 @@ import ModalLocations from './(component)/ModalLocations';
 import ModalJob from './(component)/ModalJob';
 import ModalTech from './(component)/ModalTech';
 import ModalYears from './(component)/ModalYears';
+import CompanyFilterButton from './(component)/CompanyFilterButton';
 
 export interface LocationType {
   location: string;
@@ -20,7 +21,7 @@ export interface YearsType {
 export default function CompanyPage() {
   const [currentModal, setCurrentModal] = useState<
     null | '경력' | '지역' | '직무' | '테크'
-  >('지역');
+  >(null);
   const [years, setYears] = useState<YearsType>({
     start: 1,
     end: 9,
@@ -32,42 +33,67 @@ export default function CompanyPage() {
   const onClose = () => setCurrentModal(null);
 
   return (
-    <div className='flex gap-3 p-3'>
-      <div className='relative h-[46px]'>
-        <button className='border p-2' onClick={() => setCurrentModal('지역')}>
-          지역
-        </button>
-        {currentModal === '지역' && (
-          <ModalLocations
-            data={locations}
-            setData={setLocations}
-            onClose={onClose}
-          />
-        )}
+    <div className='container flex flex-col gap-8'>
+      <div className='mt-[60px]'>
+        <h2 className='py-2 text-title font-bold'>
+          디자인 채용공고는 스팩폴리오에서
+        </h2>
+        <p className='py-2 text-subTitle'>
+          희망 정보를 선택하여 나에게 맞는 기업을 찾아보세요!
+        </p>
       </div>
-      <div className='relative h-[46px]'>
-        <button className='border p-2' onClick={() => setCurrentModal('직무')}>
-          직무
-        </button>
-        {currentModal === '직무' && (
-          <ModalJob data={job} setData={setJob} onClose={onClose} />
-        )}
+      <div className='flex items-center justify-between'>
+        <div className='flex gap-6'>
+          <div className='relative h-[46px]'>
+            <CompanyFilterButton
+              text={'지역'}
+              onClick={() => setCurrentModal('지역')}
+              selectedFilter={locations}
+            />
+            {currentModal === '지역' && (
+              <ModalLocations
+                data={locations}
+                setData={setLocations}
+                onClose={onClose}
+              />
+            )}
+          </div>
+          <div className='relative h-[46px]'>
+            <CompanyFilterButton
+              text={'직무'}
+              onClick={() => setCurrentModal('직무')}
+              selectedFilter={job}
+            />
+            {currentModal === '직무' && (
+              <ModalJob data={job} setData={setJob} onClose={onClose} />
+            )}
+          </div>
+          <div className='relative h-[46px]'>
+            <CompanyFilterButton
+              text={'테크'}
+              onClick={() => setCurrentModal('테크')}
+              selectedFilter={tech}
+            />
+            {currentModal === '테크' && (
+              <ModalTech data={tech} setData={setTech} onClose={onClose} />
+            )}
+          </div>
+          <div className='relative h-[46px]'>
+            <CompanyFilterButton
+              text={'경력'}
+              onClick={() => setCurrentModal('경력')}
+              selectedFilter={years}
+            />
+            {currentModal === '경력' && (
+              <ModalYears data={years} setData={setYears} onClose={onClose} />
+            )}
+          </div>
+        </div>
       </div>
-      <div className='relative h-[46px]'>
-        <button className='border p-2' onClick={() => setCurrentModal('테크')}>
-          테크
-        </button>
-        {currentModal === '테크' && (
-          <ModalTech data={tech} setData={setTech} onClose={onClose} />
-        )}
-      </div>
-      <div className='relative h-[46px]'>
-        <button className='border p-2' onClick={() => setCurrentModal('경력')}>
-          경력
-        </button>
-        {currentModal === '경력' && (
-          <ModalYears data={years} setData={setYears} onClose={onClose} />
-        )}
+      <div>
+        <h3 className='py-2 text-subTitle font-bold'>
+          내 포트폴리오의 컨셉으로 기업을 선택해 보세요
+        </h3>
       </div>
     </div>
   );
