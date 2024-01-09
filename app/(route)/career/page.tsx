8884bbde1e53c9389b24be0abe6dc3b5d4747319
Gahee Kim <soprue@naver.com>
2024-01-09 14:24:20 +0900

    Feat: 채용 정보 더보기 버튼 누르면 /career/company로 이동하게 구현

--- app/(route)/career/page.tsx
+++ app/(route)/career/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
+import Link from 'next/link';
 import careerBanner from '@images/career-banner.png';
 import SectionHeader from '../main/(components)/SectionHeader';
 import SectionWrapper from '../main/(components)/SectionWrapper';
@@ -10,8 +11,8 @@ import { careerCategories, portfolioCategories } from '@/constants';
 import SectionSliderWrapper from '@/(route)/main/(components)/SectionSliderWrapper';
 import MoreButton from '../main/(components)/MoreButton';
 import TechCardCarousel from './(component)/TechCard/TechCardCarousel';
-import { JobOfferDummy } from '@/dummy';
 import CompanyCardCarousel from './(component)/CompanyCard/CompanyCardCarousel';
+import { JobOfferDummy } from '@/dummy';
 
 export default function page() {
   return (
@@ -44,7 +45,9 @@ export default function page() {
             <JobOfferCard key={jobOffer.id} {...jobOffer} />
           ))}
         </CardContainer>
-        <MoreButton label='채용 정보 더보기' />
+        <Link href={`/career/company`}>
+          <MoreButton label='채용 정보 더보기' />
+        </Link>
       </SectionWrapper>
       <SectionWrapper className='bg-white'>
         <SectionHeader title='맞춤 키워드로 보는 채용공고' />
@@ -59,7 +62,9 @@ export default function page() {
             <JobOfferCard key={jobOffer.id} {...jobOffer} />
           ))}
         </CardContainer>
-        <MoreButton label='채용 정보 더보기' />
+        <Link href={`/career/company`}>
+          <MoreButton label='채용 정보 더보기' />
+        </Link>
       </SectionWrapper>
       <SectionSliderWrapper
         className='overflow-hidden bg-background-primary py-20'
