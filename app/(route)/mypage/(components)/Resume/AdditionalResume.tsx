a0f811877af6896bb4912632821feed84b2e0aea
kangkyeongseo <kks_big@naver.com>
2024-01-09 13:40:06 +0900

    Feat: Resume 컴포넌트 제작

---
+++ app/(route)/mypage/(components)/Resume/AdditionalResume.tsx
@@ -0,0 +1,15 @@
+import ResumeExperienceSection from './ResumeSection/ResumeExperienceSection';
+import ResumeAbilitySection from './ResumeSection/ResumeAbilitySection';
+import ResumePortfolioSection from './ResumeSection/ResumePortfolioSection';
+
+function AdditionalResume() {
+  return (
+    <>
+      <ResumeExperienceSection />
+      <ResumeAbilitySection />
+      <ResumePortfolioSection />
+    </>
+  );
+}
+
+export default AdditionalResume;
