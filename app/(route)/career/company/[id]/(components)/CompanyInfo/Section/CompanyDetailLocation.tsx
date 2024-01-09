9ee887279dd266467b654590520c3bcc40a057f6
kangkyeongseo <kks_big@naver.com>
2024-01-09 15:23:12 +0900

    Feat: Google Map 적용

--- app/(route)/career/company/[id]/(components)/CompanyInfo/Section/CompanyDetailLocation.tsx
+++ app/(route)/career/company/[id]/(components)/CompanyInfo/Section/CompanyDetailLocation.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { useCallback, useState } from 'react';
+import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api';
 import CompanyDetailSection from './CompanyDetailSection';
 import LocationIcon from '@/components/LocationIcon';
 
@@ -5,6 +8,7 @@ interface CompanyLocationType {
   country: string;
   region: string;
   address: string;
+  center: { lat: number; lng: number };
 }
 
 interface CompanyDetailLocationSectionPropsType {
@@ -16,6 +20,21 @@ function CompanyDetailLocationSection({
   id,
   location,
 }: CompanyDetailLocationSectionPropsType) {
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+
+  const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY || '';
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey,
+  });
+
+  const onLoad = useCallback(function cAllback(map: any) {
+    map.setZoom(15);
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback((map: any) => setMap(null), []);
+
   return (
     <CompanyDetailSection title='근무 하시게 된다면?' id={id}>
       <div className='flex gap-6 py-6 text-lg'>
@@ -27,7 +46,19 @@ function CompanyDetailLocationSection({
           <span>{location.address}</span>
         </div>
       </div>
-      <div className='w-full h-[338px] bg-background-primary rounded-lg'></div>
+      <div className='w-full h-[338px] bg-background-primary rounded-lg overflow-hidden'>
+        {isLoaded && (
+          <GoogleMap
+            mapContainerStyle={{ width: '100%', height: '100%' }}
+            center={location.center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            <MarkerF key={location.address} position={location.center} />
+          </GoogleMap>
+        )}
+      </div>
     </CompanyDetailSection>
   );
 }
