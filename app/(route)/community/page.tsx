70552079de9592d678eda3900683ca1385b677bf
Gahee Kim <soprue@naver.com>
2024-01-09 15:58:29 +0900

    Feat: 커뮤니티 배너 컴포넌트 구현

---
+++ app/(route)/community/page.tsx
@@ -0,0 +1,16 @@
+import CommunityBanner from './(components)/CommunityBanner';
+
+export default function page() {
+  return (
+    <>
+      {/* main banner */}
+      <CommunityBanner />
+
+      {/* side section */}
+      <section></section>
+
+      {/* post article */}
+      <article></article>
+    </>
+  );
+}
