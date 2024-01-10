f602feb73a1e574e4d53b5eca07642f4eb33d64d
kangkyeongseo <kks_big@naver.com>
2024-01-10 15:48:15 +0900

    Feat: 포트폴리오 상세 페이지 제작

--- app/(route)/portfolio/@modal/(.)[id]/page.tsx
+++ app/(route)/portfolio/@modal/(.)[id]/page.tsx
@@ -1,13 +1,13 @@
 import Modal from '@/(route)/portfolio/[id]/(components)/Modal';
+import PortfolioDetail from '../../[id]/(components)/PortfolioDetail/PortfolioDetail';
+import PortfolioInfo from '../../[id]/(components)/PortfolioInfo/PortfolioInfo';
 
 function PortfolioDetailPage() {
   return (
     <Modal>
       <div className='w-fit h-fit flex gap-[25px]'>
-        <div className='w-[845px] h-[500vh] bg-white'>Portfolio Image</div>
-        <div className='overflow-auto sticky top-0 w-[390px] h-[1000px] bg-white'>
-          <div className='h-[200vh]'>Portfolio Info</div>
-        </div>
+        <PortfolioDetail />
+        <PortfolioInfo />
       </div>
     </Modal>
   );
