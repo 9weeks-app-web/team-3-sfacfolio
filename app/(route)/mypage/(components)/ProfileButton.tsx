8000f6cec769bebd3434dab1078fdf2944b00cd3
seonnn <ssni0105@gmail.com>
2024-01-10 17:50:26 +0900

    Refactor: ProfileButton 컴포넌트 props 수정

--- app/(route)/mypage/(components)/ProfileButton.tsx
+++ app/(route)/mypage/(components)/ProfileButton.tsx
@@ -1,4 +1,6 @@
+'use client';
 import React from 'react';
+import { ProfileTabMenusType } from './ProfileDetail';
 
 const profileButtonOption = {
   black: 'bg-text-normal text-white',
@@ -8,13 +10,18 @@ const profileButtonOption = {
 function ProfileButton({
   text,
   option = 'black',
+  setCurrentTab,
 }: {
   text: string;
   option?: 'black' | 'white';
+  setCurrentTab?: React.Dispatch<
+    React.SetStateAction<ProfileTabMenusType | null>
+  >;
 }) {
   return (
     <button
       className={`h-12 w-[302px] rounded-lg font-bold leading-4 ${profileButtonOption[option]}`}
+      onClick={() => setCurrentTab && setCurrentTab(null)}
     >
       {text}
     </button>
