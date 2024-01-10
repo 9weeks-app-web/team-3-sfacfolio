3319498f4d46db2d1edda0d4f4950b203375d7fd
Gahee Kim <soprue@naver.com>
2024-01-10 19:02:22 +0900

    Refactor: sfac quiz dummy data 구현

---
+++ app/dummy/spacQuiz.ts
@@ -0,0 +1,85 @@
+import SpacQuiz1 from '@images/SpacQuiz/SpacQuiz1.png';
+import SpacQuiz2 from '@images/SpacQuiz/SpacQuiz2.png';
+import SpacQuiz3 from '@images/SpacQuiz/SpacQuiz3.png';
+import SpacQuiz4 from '@images/SpacQuiz/SpacQuiz4.png';
+import SpacQuiz5 from '@images/SpacQuiz/SpacQuiz5.png';
+import SpacQuiz6 from '@images/SpacQuiz/SpacQuiz6.png';
+import SpacQuiz7 from '@images/SpacQuiz/SpacQuiz7.png';
+import SpacQuiz8 from '@images/SpacQuiz/SpacQuiz8.png';
+import SpacQuiz9 from '@images/SpacQuiz/SpacQuiz9.png';
+import SpacQuiz10 from '@images/SpacQuiz/SpacQuiz10.png';
+import SpacQuiz11 from '@images/SpacQuiz/SpacQuiz11.png';
+import SpacQuiz12 from '@images/SpacQuiz/SpacQuiz12.png';
+
+export const SpacQuizDummy = [
+  {
+    title: ['모바일 환경에', ' 적절한 것은? (2)'],
+    date: '2023년 1월 둘째 주',
+    image: SpacQuiz1,
+  },
+  {
+    title: ['커뮤니티 환경에', ' 적절한 것은?'],
+    date: '2023년 8월 셋째 주',
+    image: SpacQuiz2,
+  },
+  {
+    title: ['웹 환경에', ' 적절한 것은? (2)'],
+    date: '2023년 7월 둘째 주',
+    image: SpacQuiz3,
+  },
+  {
+    title: ['미디어 플랫폼에', ' 적절한 것은?'],
+    date: '2023년 6월 둘째 주',
+    image: SpacQuiz4,
+  },
+  {
+    title: ['동기부여 플랫폼에', ' 적절한 것은?'],
+    date: '2023년 6월 첫째 주',
+    image: SpacQuiz5,
+  },
+  {
+    title: ['환경 설정에', ' 적절한 것은?'],
+    date: '2023년 6월 첫째 주',
+    image: SpacQuiz6,
+  },
+  {
+    title: ['웹 환경에', ' 적절한 것은? (1)'],
+    date: '2023년 5월 둘째 주',
+    image: SpacQuiz7,
+  },
+  {
+    title: ['모바일 환경에', ' 적절한 것은? (1)'],
+    date: '2023년 5월 첫째 주',
+    image: SpacQuiz8,
+  },
+  {
+    title: ['쇼핑 플랫폼에', ' 적절한 것은? (1)'],
+    date: '2023년 4월 둘째 주',
+    image: SpacQuiz9,
+  },
+  {
+    title: ['금융 서비스에', ' 적절한 것은?'],
+    date: '2023년 3월 둘째 주',
+    image: SpacQuiz10,
+  },
+  {
+    title: ['에러 피드백,', ' 적절한 것은? (2)'],
+    date: '2023년 3월 첫째 주',
+    image: SpacQuiz11,
+  },
+  {
+    title: ['에러 피드백,', ' 적절한 것은? (1)'],
+    date: '2023년 2월 첫째 주',
+    image: SpacQuiz12,
+  },
+  {
+    title: ['에러 피드백,', ' 적절한 것은? (2)'],
+    date: '2023년 3월 첫째 주',
+    image: SpacQuiz11,
+  },
+  {
+    title: ['에러 피드백,', ' 적절한 것은? (1)'],
+    date: '2023년 2월 첫째 주',
+    image: SpacQuiz12,
+  },
+];
