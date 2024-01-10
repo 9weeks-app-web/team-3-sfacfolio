f52ecc8419dbd5bf4c0bc1a288364c5c1a007a86
Gahee Kim <soprue@naver.com>
2024-01-10 15:56:05 +0900

    Feat: 필터 카테고리 버튼 추가 (기능 구현 x)

--- app/(route)/community/(components)/CommunityPostList.tsx
+++ app/(route)/community/(components)/CommunityPostList.tsx
@@ -1,6 +1,9 @@
-import { CommunityPostType } from '@/types';
 import CommunityPostBox from './CommunityPostBox';
 import CommunityWriteButton from './CommunityWriteButton';
+import CategoryButtonWrap from '@/(route)/main/(components)/CategoryButtonWrap';
+
+import { CommunityPostType } from '@/types';
+import { communityFilterCategories } from '@/constants';
 
 interface CommunityPostListProps {
   currentMenu: string;
@@ -13,15 +16,30 @@ export default function CommunityPostList({
 }: CommunityPostListProps) {
   return (
     <div>
-      <div className='flex items-center justify-between py-4'>
+      <div className='flex items-center justify-between pt-4'>
         <p className='text-title font-bold'>{currentMenu}</p>
         <CommunityWriteButton />
       </div>
 
-      <div className='flex flex-col gap-6'>
-        {posts.map((post: CommunityPostType, i: number) => (
-          <CommunityPostBox post={post} key={i} />
-        ))}
+      {currentMenu !== '실시간 인기 글' && (
+        <CategoryButtonWrap
+          type='button'
+          gradient=' to-background-primary'
+          categories={communityFilterCategories}
+          className=''
+        />
+      )}
+
+      <div className='flex flex-col gap-6 pt-4'>
+        {posts.length === 0 ? (
+          <div className='w-full rounded-lg border border-line-normal px-[22px] py-4 text-text-alternative'>
+            게시글이 없습니다.
+          </div>
+        ) : (
+          posts.map((post: CommunityPostType, i: number) => (
+            <CommunityPostBox post={post} key={i} />
+          ))
+        )}
       </div>
 
       <div className='mt-6 flex items-center justify-end'>
