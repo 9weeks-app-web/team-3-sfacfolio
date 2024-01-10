4e8df03d1f071313b12430b2a9fa4cb3f9bc1c08
geonwooPark <white0581@naver.com>
2024-01-10 17:00:46 +0900

    Chore: JobOffer 더미 데이터 logo 추가

--- app/(route)/main/(components)/Card/JobOfferCard/JobOfferCardFooter.tsx
+++ app/(route)/main/(components)/Card/JobOfferCard/JobOfferCardFooter.tsx
@@ -1,16 +1,19 @@
+import Image from 'next/image';
 import React from 'react';
 
 interface JobOfferCardFooterProps {
   company: string;
+  logo: string;
 }
 
 export default function JobOfferCardFooter({
   company,
+  logo,
 }: JobOfferCardFooterProps) {
   return (
     <div className='flex items-center'>
-      <div className='mr-3 h-10 w-10 rounded-full bg-[#d9d9d9]'>
-        {/* <Image src={} alt='company-image' fill objectFit='cover' /> */}
+      <div className='relative mr-3 h-10 w-10 overflow-hidden rounded-full bg-[#d9d9d9]'>
+        <Image src={logo} alt='company-image' fill objectFit='cover' />
       </div>
       <p className='text-label2 font-medium text-text-normal'>{company}</p>
     </div>
