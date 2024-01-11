790b691e43346f2b10d71e0a43898930e75ec259
seonnn <ssni0105@gmail.com>
2024-01-11 15:14:38 +0900

    Refactor: SectionHeader 컴포넌트 props 수정 및 style 수정

--- app/(route)/main/(components)/SectionHeader.tsx
+++ app/(route)/main/(components)/SectionHeader.tsx
@@ -4,17 +4,28 @@ import MoreButton from '@images/MoreButton.svg';
 
 interface SectionHeaderProps {
   title: string;
-  more?: any;
+  subTitle?: string;
+  more?: string;
   className?: string;
 }
 
-function SectionHeader({ title, more, className = '' }: SectionHeaderProps) {
+function SectionHeader({
+  subTitle,
+  title,
+  more,
+  className = '',
+}: SectionHeaderProps) {
   return (
     <div className={`flex items-center justify-between ${className}`}>
-      <p className='text-title font-bold '>{title}</p>
+      <div className='flex flex-col gap-2.5'>
+        <h3 className='text-subTitle leading-6 text-primary-heavy'>
+          {subTitle}
+        </h3>
+        <h2 className='text-title font-bold leading-7'>{title}</h2>
+      </div>
       {more && (
         <Link
-          href={more}
+          href={{ pathname: more }}
           className='flex items-center text-[18px] text-text-alternative'
         >
           더보기
