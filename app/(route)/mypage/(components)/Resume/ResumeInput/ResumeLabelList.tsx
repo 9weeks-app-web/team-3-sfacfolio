966dc9b2b511d9890b938c3d7fd23b930f50167d
Gahee Kim <soprue@naver.com>
2024-01-11 14:13:57 +0900

    Update: ResumeLabelList 컴포넌트 수정

--- app/(route)/mypage/(components)/Resume/ResumeInput/ResumeLabelList.tsx
+++ app/(route)/mypage/(components)/Resume/ResumeInput/ResumeLabelList.tsx
@@ -1,20 +1,18 @@
 interface ResumeLabelListPropsType {
   label: string;
-  labelList: { image?: string; name: string }[];
+  labelList: { name: string }[];
 }
 
 function ResumeLabelList({ label, labelList }: ResumeLabelListPropsType) {
   return (
-    <div className='flex flex-col gap-3 col-span-4'>
+    <div className='col-span-4 flex flex-col gap-3'>
       <label className='text-lg font-bold text-text-alternative'>{label}</label>
       <div className='flex gap-2'>
-        {labelList.map(label => (
-          <div className='flex items-center gap-1 px-3 py-2 rounded-full border'>
-            {label.image ? (
-              <div></div>
-            ) : (
-              <div className='w-6 h-6 rounded-full bg-[#d9d9d9]' />
-            )}
+        {labelList.map((label, i) => (
+          <div
+            className='flex items-center gap-1 rounded-full border px-3 py-2'
+            key={i}
+          >
             <span>{label.name}</span>
           </div>
         ))}
