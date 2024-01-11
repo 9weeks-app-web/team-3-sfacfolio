8cb698669c1f614e757d91f37c0f5ed51f39aab3
Gahee Kim <soprue@naver.com>
2024-01-11 14:12:19 +0900

    Feat: 마이페이지 커리어 탭 레이아웃 구현

--- app/(route)/mypage/(components)/contents/ProfileCareer.tsx
+++ app/(route)/mypage/(components)/contents/ProfileCareer.tsx
@@ -1,3 +1,21 @@
-export default function ProfileCareer() {
-  return <div>ProfileCareer</div>;
+import { JobOfferDummy } from '@/dummy';
+import ApplyStatus from '../ApplyStatus';
+import { ProfileContentsProps } from './ProfileCommunity';
+import JobOfferCard from '@/(route)/main/(components)/Card/JobOfferCard/JobOfferCard';
+import Resume from '../Resume/Resume';
+
+export default function ProfileCareer({ currentSubTab }: ProfileContentsProps) {
+  return (
+    <div className='mt-6'>
+      {currentSubTab === '지원 현황' && <ApplyStatus />}
+      {currentSubTab === '관심 공고' && (
+        <div className='grid grid-cols-3 gap-2'>
+          {JobOfferDummy.slice(0, 6).map(jobOffer => (
+            <JobOfferCard key={jobOffer.id} {...jobOffer} />
+          ))}
+        </div>
+      )}
+      {currentSubTab === '이력서 관리' && <Resume />}
+    </div>
+  );
 }
